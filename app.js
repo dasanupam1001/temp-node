c45c9cc16ce90926c1228446f7a93ce24bab090c
Anupam <dasanupam1001@gmail.com>
2023-08-21 12:37:35 +0530

    13-lodash.js file creation

--- app.js
+++
@@ -1,6 +0,0 @@
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4, 5], 6]]]
-const newItems = _.flattenDeep(items) // to get a single array from the nested array
-console.log(newItems)
-console.log('hello world!')
